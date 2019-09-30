811c5131ff0428c5139359e38be0a765230213c5
Aleksey Yakob <jayleks@mail.ru>
2019-09-30 13:05:33 +0300

    added data folder

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyTest.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+
+        public AppDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=.;Database=CompanyTest;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Company>(e => 
+            {
+                e.HasKey(q => q.Id);
+                e.Property(q => q.Id).ValueGeneratedNever();
+                e.Property(q => q.Name).IsRequired();
+            });
+        }
+    }
+}
